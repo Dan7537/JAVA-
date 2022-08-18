1b68a52cd782f025b2445db06594bc5c87fefff6
wolflo <londonolondonoj@gmail.com>
2022-08-18 15:29:51 -0500

    MYfirstcommit

---
+++ src/myfirstproject/Main.java
@@ -0,0 +1,69 @@
+package myfirstproject;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		
+//		double ageJuan = 9;
+//		double ageAlberto = (2)*ageJuan/3;
+//		double ageAna = (4)*ageJuan/3;
+//		double ageMother = ageJuan + ageAlberto + ageAna;
+//		
+//
+//		System.out.prdoubleln("La edad de Juan es " +ageJuan);
+//		System.out.prdoubleln("La edad de Alberto es " + ageAlberto);
+//		System.out.prdoubleln("La edad de Ana es " + ageAna);
+//		System.out.prdoubleln("La edad de la mama es " + ageMother);
+//		
+//		System.out.prdoubleln("LAS EDADES SON: ALBERTO = " + ageAlberto +
+//				" JUAN = "+ ageJuan + " ANA = " + ageAna + " MAMA = "+ ageMother);
+
+		
+		/*
+		 double SUMA = 0;
+		 double X = 20;
+		  SUMA = SUMA + X;
+		 double Y = 40;
+		  X = X + Math.pow(Y, 2);
+		  SUMA = SUMA + (X / Y);
+		 System.out.println("EL VALOR DE LA SUMA ES:" + SUMA); 
+		*/
+		
+		/*
+		double horas =48;
+		double preciohora = 5000;
+		double salBruto = horas * preciohora;
+		double porcRetencion = 0.125;
+		double salNeto = salBruto - (salBruto * porcRetencion);
+		System.out.println("El salario bruto es: "+ salBruto);
+		System.out.println("La retencion en la fuente es: "+ salBruto * porcRetencion);
+		System.out.println("El salario neto es: "+ salNeto);
+		
+		
+		*/
+		
+		/*
+		
+		double num = 7;
+		double cuadrado= Math.pow(num, 2);
+		double cubo = Math.pow(num, 3);
+		System.out.println("El cuadrado de "+ num +" es "+cuadrado);
+		System.out.println("El cubo de "+num+" es "+cubo);
+		
+		*/
+		
+		
+		double radio = 5;
+		double area = Math.PI * Math.pow(radio,2);
+		double longcircunfe= 2*Math.PI*radio;
+		System.out.println("El area es: "+ area);
+		System.out.println("La longitud de la circunferencia es: "+ longcircunfe);
+		
+		
+		
+		
+		
+	}
+
+}
